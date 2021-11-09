ea3b1dd5eaec60764ee4afcbbf6d6e907b07b58f
heidichen90 <toddypet@gmail.com>
2021-11-09 00:23:16 +0000

    set up sign up page

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-const express = require('express')
-const handlebars = require('express-handlebars')
-const db = require('./models')
-const app = express()
-const port = 3000
+const express = require("express");
+const handlebars = require("express-handlebars");
+const db = require("./models");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
 
-app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  db.sequelize.sync();
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 
-require('./routes')(app)
+require("./routes")(app);
 
-module.exports = app
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,19 @@
-const restControllers = require('../controllers/restControllers')
-const adminController = require('../controllers/adminControllers')
+const restControllers = require("../controllers/restController");
+const adminController = require("../controllers/adminController");
+const userController = require("../controllers/userController");
 
 module.exports = (app) => {
   // 如果使用者訪問首頁，就導向 /restaurants 的頁面
-  app.get('/', (req, res) => {
-    res.redirect('restaurants')
-  })
-  app.get('/restaurants', restControllers.getRestaurants)
-  app.get('/admin', (req, res) => {
-    res.redirect('/admin/restaurants')
-  })
-  app.get('/admin/restaurants', adminController.getRestaurants)
-}
+  app.get("/", (req, res) => {
+    res.redirect("restaurants");
+  });
+  app.get("/restaurants", restControllers.getRestaurants);
+
+  app.get("/admin", (req, res) => {
+    res.redirect("/admin/restaurants");
+  });
+  app.get("/admin/restaurants", adminController.getRestaurants);
+
+  app.get("/signup", userController.signUpPage);
+  app.post("/signup", userController.signUp);
+};
