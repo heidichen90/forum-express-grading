bc1dff52b6a67aeb1f88c7c9c985bd0ac2dc54ba
heidichen90 <toddypet@gmail.com>
2021-11-10 23:46:52 +0000

    correct styling

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,57 +1,57 @@
-const db = require("../models");
-const Restaurant = db.Restaurant;
+const db = require('../models')
+const Restaurant = db.Restaurant
 
 const adminController = {
   getRestaurants: (req, res) => {
     return Restaurant.findAll({ raw: true, nest: true }).then((restaurants) => {
-      return res.render("admin/restaurants", { restaurants: restaurants });
-    });
+      return res.render('admin/restaurants', { restaurants: restaurants })
+    })
   },
 
   createRestaurants: (req, res) => {
-    return res.render("admin/create");
+    return res.render('admin/create')
   },
 
   postRestaurants: (req, res) => {
     if (!req.body.name) {
-      req.flash("error_messages", "name didn't exist");
-      return res.redirect("back");
+      req.flash('error_messages', "name didn't exist")
+      return res.redirect('back')
     }
-    const { name, tel, address, opening_hours, description } = req.body;
+    const { name, tel, address, opening_hours, description } = req.body
     return Restaurant.create({
       name,
       tel,
       address,
       opening_hours,
-      description,
+      description
     }).then((restaurant) => {
-      req.flash("success_messages", "restaurant was successfully created");
-      return res.redirect("/admin/restaurants");
-    });
+      req.flash('success_messages', 'restaurant was successfully created')
+      return res.redirect('/admin/restaurants')
+    })
   },
 
   getRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id, { raw: true, nest: true }).then(
       (restaurant) => {
-        return res.render("admin/restaurant", {
-          restaurant,
-        });
+        return res.render('admin/restaurant', {
+          restaurant
+        })
       }
-    );
+    )
   },
 
   editRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id, { raw: true, nest: true }).then(
       (restaurant) => {
-        return res.render("admin/create", { restaurant });
+        return res.render('admin/create', { restaurant })
       }
-    );
+    )
   },
 
   putRestaurant: (req, res) => {
     if (!req.body.name) {
-      req.flash("error_message", "name didnt exit");
-      return res.redirect("back");
+      req.flash('error_message', 'name didnt exit')
+      return res.redirect('back')
     }
     return Restaurant.findByPk(req.params.id).then((restaurant) => {
       restaurant
@@ -60,25 +60,25 @@ const adminController = {
           tel: req.body.tel,
           address: req.body.address,
           opening_hours: req.body.opening_hours,
-          description: req.body.description,
+          description: req.body.description
         })
         .then((restaurant) => {
           req.flash(
-            "success_messages",
-            "restaurant was successfully to update"
-          );
-          res.redirect("/admin/restaurants");
-        });
-    });
+            'success_messages',
+            'restaurant was successfully to update'
+          )
+          res.redirect('/admin/restaurants')
+        })
+    })
   },
 
   deleteRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id).then((restaurant) => {
       restaurant.destroy().then((restaurant) => {
-        res.redirect("/admin/restaurants");
-      });
-    });
-  },
-};
+        res.redirect('/admin/restaurants')
+      })
+    })
+  }
+}
 
-module.exports = adminController;
+module.exports = adminController
--- routes/index.js
+++ routes/index.js
@@ -1,83 +1,83 @@
-const restControllers = require("../controllers/restController");
-const adminController = require("../controllers/adminController");
-const userController = require("../controllers/userController");
-const passport = require("passport");
+const restControllers = require('../controllers/restController')
+const adminController = require('../controllers/adminController')
+const userController = require('../controllers/userController')
+const passport = require('passport')
 
 module.exports = (app) => {
   const authenticated = (req, res, next) => {
     if (req.isAuthenticated()) {
-      return next();
+      return next()
     }
-    res.redirect("/signin");
-  };
+    res.redirect('/signin')
+  }
 
   const authenticatedAdmin = (req, res, next) => {
     if (req.isAuthenticated()) {
-      console.log(req.user);
+      console.log(req.user)
       if (req.user.isAdmin) {
-        return next();
+        return next()
       }
-      return res.redirect("/");
+      return res.redirect('/')
     }
-    res.redirect("/signin");
-  };
+    res.redirect('/signin')
+  }
 
   // 如果使用者訪問首頁，就導向 /restaurants 的頁面
-  app.get("/", authenticated, (req, res) => {
-    res.redirect("restaurants");
-  });
-  app.get("/restaurants", restControllers.getRestaurants);
+  app.get('/', authenticated, (req, res) => {
+    res.redirect('restaurants')
+  })
+  app.get('/restaurants', restControllers.getRestaurants)
 
-  app.get("/admin", authenticatedAdmin, (req, res) => {
-    res.redirect("/admin/restaurants");
-  });
+  app.get('/admin', authenticatedAdmin, (req, res) => {
+    res.redirect('/admin/restaurants')
+  })
   app.get(
-    "/admin/restaurants",
+    '/admin/restaurants',
     authenticatedAdmin,
     adminController.getRestaurants
-  );
+  )
 
-  app.get("/signup", userController.signUpPage);
-  app.post("/signup", userController.signUp);
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', userController.signUp)
 
-  app.get("/signin", userController.signInPage);
+  app.get('/signin', userController.signInPage)
   app.post(
-    "/signin",
-    passport.authenticate("local", {
-      failureRedirect: "/signin",
-      failureFlash: true,
+    '/signin',
+    passport.authenticate('local', {
+      failureRedirect: '/signin',
+      failureFlash: true
     }),
     userController.signIn
-  );
-  app.get("/logout", userController.logout);
+  )
+  app.get('/logout', userController.logout)
   app.get(
-    "/admin/restaurants/create",
+    '/admin/restaurants/create',
     authenticatedAdmin,
     adminController.createRestaurants
-  );
+  )
   app.post(
-    "/admin/restaurants",
+    '/admin/restaurants',
     authenticatedAdmin,
     adminController.postRestaurants
-  );
+  )
   app.get(
-    "/admin/restaurants/:id",
+    '/admin/restaurants/:id',
     authenticatedAdmin,
     adminController.getRestaurant
-  );
+  )
   app.get(
-    "/admin/restaurants/:id/edit",
+    '/admin/restaurants/:id/edit',
     authenticatedAdmin,
     adminController.editRestaurant
-  );
+  )
   app.put(
-    "/admin/restaurants/:id",
+    '/admin/restaurants/:id',
     authenticatedAdmin,
     adminController.putRestaurant
-  );
+  )
   app.delete(
-    "/admin/restaurants/:id",
+    '/admin/restaurants/:id',
     authenticatedAdmin,
     adminController.deleteRestaurant
-  );
-};
+  )
+}
