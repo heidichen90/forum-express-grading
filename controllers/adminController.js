8aae24dfca520826b8bcf2b1e0089189a9818065
heidichen90 <toddypet@gmail.com>
2021-11-10 23:12:54 +0000

    view one restaurant

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -7,6 +7,38 @@ const adminController = {
       return res.render("admin/restaurants", { restaurants: restaurants });
     });
   },
+
+  createRestaurants: (req, res) => {
+    return res.render("admin/create");
+  },
+
+  postRestaurants: (req, res) => {
+    if (!req.body.name) {
+      req.flash("error_messages", "name didn't exist");
+      return res.redirect("back");
+    }
+    const { name, tel, address, opening_hours, description } = req.body;
+    return Restaurant.create({
+      name,
+      tel,
+      address,
+      opening_hours,
+      description,
+    }).then((restaurant) => {
+      req.flash("success_messages", "restaurant was successfully created");
+      return res.redirect("/admin/restaurants");
+    });
+  },
+
+  getRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id, { raw: true, nest: true }).then(
+      (restaurant) => {
+        return res.render("admin/restaurant", {
+          restaurant: restaurant,
+        });
+      }
+    );
+  },
 };
 
 module.exports = adminController;
