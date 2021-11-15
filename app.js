093f8cebac736f6db28ebbbef2bd54c4244247f9
heidichen90 <toddypet@gmail.com>
2021-11-15 23:27:59 +0000

    add helpers to set selected category

--- app.js
+++ app.js
@@ -1,41 +1,47 @@
-const express = require('express')
-const handlebars = require('express-handlebars')
-const db = require('./models')
-const flash = require('connect-flash')
-const session = require('express-session')
-const passport = require('./config/passport')
-const methodOverride = require('method-override')
+const express = require("express");
+const handlebars = require("express-handlebars");
+const db = require("./models");
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("./config/passport");
+const methodOverride = require("method-override");
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
-app.use(express.urlencoded({ extended: true }))
-app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(flash())
-app.use(methodOverride('_method'))
-app.use('/upload', express.static(__dirname + '/upload'))
+app.engine(
+  "handlebars",
+  handlebars({
+    defaultLayout: "main",
+    helpers: require("./config/handlebars-helpers"),
+  })
+);
+app.set("view engine", "handlebars");
+app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: "secret", resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use(methodOverride("_method"));
+app.use("/upload", express.static(__dirname + "/upload"));
 
 app.use((req, res, next) => {
-  res.locals.success_messages = req.flash('success_messages')
-  res.locals.error_messages = req.flash('error_messages')
-  res.locals.user = req.user
-  next()
-})
+  res.locals.success_messages = req.flash("success_messages");
+  res.locals.error_messages = req.flash("error_messages");
+  res.locals.user = req.user;
+  next();
+});
 
 app.listen(port, () => {
   // what is this for?
-  db.sequelize.sync()
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  db.sequelize.sync();
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 
-require('./routes')(app)
+require("./routes")(app);
 
-module.exports = app
+module.exports = app;
