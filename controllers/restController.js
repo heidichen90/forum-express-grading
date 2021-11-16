eeaaa2d3c5122ba514a67dc48f35ee2b9ae47c20
heidichen90 <toddypet@gmail.com>
2021-11-16 23:58:03 +0000

    add pagination feature

--- controllers/restController.js
+++ controllers/restController.js
@@ -2,34 +2,57 @@ const db = require("../models");
 const Restaurant = db.Restaurant;
 const Category = db.Category;
 
+const pageLimit = 10;
+
 const restControllers = {
   getRestaurants: (req, res) => {
+    let offset = 0;
     const whereQuery = {};
     let categoryId = "";
+
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId);
       whereQuery.CategoryId = categoryId;
     }
-    Restaurant.findAll({
-      raw: true,
-      nest: true,
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit;
+    }
+
+    Restaurant.findAndCountAll({
       include: Category,
       where: whereQuery,
+      offset: offset,
+      limit: pageLimit,
     }).then((restaurants) => {
-      const data = restaurants.map((r) => ({
-        ...r,
-        description: r.description.substring(0, 50),
-        categoryName: r.Category.name,
+      //data for pagination
+      const page = Number(req.query.page) || 1;
+      const pages = Math.ceil(restaurants.count / pageLimit);
+      const totalPage = Array.from({ length: pages }).map(
+        (item, index) => index + 1
+      );
+
+      const prev = page - 1 < 1 ? 1 : page - 1;
+      const next = page + 1 > pages ? pages : page + 1;
+
+      const data = restaurants.rows.map((r) => ({
+        ...r.dataValues,
+        description: r.dataValues.description.substring(0, 50),
+        categoryName: r.dataValues.Category.name,
       }));
       Category.findAll({ raw: true, nest: true }).then((categories) => {
         return res.render("restaurants", {
           restaurants: data,
           categories,
           categoryId,
+          page,
+          totalPage,
+          prev,
+          next,
         });
       });
     });
   },
+
   getRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id, {
       include: Category,
