2800f4d9f1a4509cd7ad989ce509f295f6c21e5d
heidichen90 <toddypet@gmail.com>
2021-11-16 21:37:32 +0000

    fix javascript styling

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,6 @@
-const db = require("../models");
-const Restaurant = db.Restaurant;
-const Category = db.Category;
+const db = require('../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
 
 const restControllers = {
   getRestaurants: (req, res) => {
@@ -9,21 +9,21 @@ const restControllers = {
         const data = restaurants.map((r) => ({
           ...r,
           description: r.description.substring(0, 50),
-          categoryName: r.Category.name,
-        }));
-        return res.render("restaurants", { restaurants: data });
+          categoryName: r.Category.name
+        }))
+        return res.render('restaurants', { restaurants: data })
       }
-    );
+    )
   },
   getRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category,
+      include: Category
     }).then((restaurant) => {
-      return res.render("restaurant", {
-        restaurant: restaurant.toJSON(),
-      });
-    });
-  },
-};
+      return res.render('restaurant', {
+        restaurant: restaurant.toJSON()
+      })
+    })
+  }
+}
 
-module.exports = restControllers;
+module.exports = restControllers
