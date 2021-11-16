7ed0de0397a740550c821b46eb59ef158bb25b4a
heidichen90 <toddypet@gmail.com>
2021-11-16 23:26:31 +0000

    filter restaurant list

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,29 +1,44 @@
-const db = require('../models')
-const Restaurant = db.Restaurant
-const Category = db.Category
+const db = require("../models");
+const Restaurant = db.Restaurant;
+const Category = db.Category;
 
 const restControllers = {
   getRestaurants: (req, res) => {
-    Restaurant.findAll({ raw: true, nest: true, include: Category }).then(
-      (restaurants) => {
-        const data = restaurants.map((r) => ({
-          ...r,
-          description: r.description.substring(0, 50),
-          categoryName: r.Category.name
-        }))
-        return res.render('restaurants', { restaurants: data })
-      }
-    )
+    const whereQuery = {};
+    let categoryId = "";
+    if (req.query.categoryId) {
+      categoryId = Number(req.query.categoryId);
+      whereQuery.CategoryId = categoryId;
+    }
+    Restaurant.findAll({
+      raw: true,
+      nest: true,
+      include: Category,
+      where: whereQuery,
+    }).then((restaurants) => {
+      const data = restaurants.map((r) => ({
+        ...r,
+        description: r.description.substring(0, 50),
+        categoryName: r.Category.name,
+      }));
+      Category.findAll({ raw: true, nest: true }).then((categories) => {
+        return res.render("restaurants", {
+          restaurants: data,
+          categories,
+          categoryId,
+        });
+      });
+    });
   },
   getRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category
+      include: Category,
     }).then((restaurant) => {
-      return res.render('restaurant', {
-        restaurant: restaurant.toJSON()
-      })
-    })
-  }
-}
+      return res.render("restaurant", {
+        restaurant: restaurant.toJSON(),
+      });
+    });
+  },
+};
 
-module.exports = restControllers
+module.exports = restControllers;
