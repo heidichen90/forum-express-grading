164fede27906bfe929b54a5e86704618686737f2
heidichen90 <toddypet@gmail.com>
2021-11-22 00:22:04 +0000

    fix the type on deletecategory

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,54 +1,54 @@
-const db = require('../models')
-const Category = db.Category
+const db = require("../models");
+const Category = db.Category;
 const categoryController = {
   getCategories: (req, res) => {
     return Category.findAll({
       raw: true,
-      nest: true
+      nest: true,
     }).then((categories) => {
       if (req.params.id) {
         Category.findByPk(req.params.id).then((category) => {
           if (category) {
-            return res.render('admin/categories', {
+            return res.render("admin/categories", {
               categories,
-              category: category.toJSON()
-            })
+              category: category.toJSON(),
+            });
           }
-        })
+        });
       } else {
-        return res.render('admin/categories', { categories })
+        return res.render("admin/categories", { categories });
       }
-    })
+    });
   },
   postCategory: (req, res) => {
     if (!req.body.name) {
-      req.flash('error_messages', "name didn't exist")
-      return res.redirect('back')
+      req.flash("error_messages", "name didn't exist");
+      return res.redirect("back");
     } else {
       return Category.create({ name: req.body.name }).then((category) => {
-        res.redirect('/admin/categories')
-      })
+        res.redirect("/admin/categories");
+      });
     }
   },
   putCategory: (req, res) => {
     if (!req.body.name) {
-      req.flash('error_messages', "name didn't exist")
-      return res.redirect('back')
+      req.flash("error_messages", "name didn't exist");
+      return res.redirect("back");
     } else {
       return Category.findByPk(req.params.id).then((category) => {
         category
           .update(req.body)
-          .then((category) => res.redirect('/admin/categories'))
-      })
+          .then((category) => res.redirect("/admin/categories"));
+      });
     }
   },
-  deletCategory: (req, res) => {
+  deleteCategory: (req, res) => {
     return Category.findByPk(req.params.id).then((category) => {
       category.destroy().then((category) => {
-        res.redirect('/admin/categories')
-      })
-    })
-  }
-}
+        res.redirect("/admin/categories");
+      });
+    });
+  },
+};
 
-module.exports = categoryController
+module.exports = categoryController;
