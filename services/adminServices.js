42a27ed366eebdc2e1ad8fb7bf5c4e8749710318
heidichen90 <toddypet@gmail.com>
2021-11-25 22:38:32 +0000

    refactor adminController.getRestaurant

--- services/adminServices.js
+++ services/adminServices.js
@@ -15,6 +15,17 @@ const adminServices = {
       console.log(error);
     }
   },
+  getRestaurant: async (req, res, callback) => {
+    try {
+      const restaurant = await Restaurant.findByPk(req.params.id, {
+        include: [Category],
+      });
+
+      return callback({ restaurant: restaurant.toJSON() });
+    } catch (error) {
+      console.log(errror);
+    }
+  },
 };
 
 module.exports = adminServices;
